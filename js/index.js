65fa30513d47f8e89da4096b91940f7ba86562f0
Taras Yakymyuk <jakimuk.taras.semenowuch@gmail.com>
2018-05-15 16:05:56 +0000

    css addons

--- js/index.js
+++ js/index.js
@@ -4,13 +4,13 @@
     fetch(API_URL_POL)
       .then(response => response.json())
       .then(data => {
-        document.querySelector('.poloniex_btc').innerText =  data.USD;
+        document.querySelector('.poloniex_btc').innerText = data.USD;
       })
     const API_URL_HIT = 'https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD&e=HitBtc&extraParams=arbitrager';
     fetch(API_URL_HIT)
       .then(response => response.json())
       .then(data => {
-        document.querySelector('.hitbtc_btc').innerText =  data.USD;
+        document.querySelector('.hitbtc_btc').innerText = data.USD;
       })
   }
 
@@ -72,7 +72,7 @@
     fetch(API_URL_HIT)
       .then(response => response.json())
       .then(data => {
-        document.querySelector('.hitbtc_ltc').innerText = data.USD  ;
+        document.querySelector('.hitbtc_ltc').innerText = data.USD;
       })
   }
   fromLtcToUsd();
@@ -80,77 +80,77 @@
 
 
 
-function calculateDiff1() {
-  const num1 = parseFloat(document.querySelector('.poloniex_btc').innerHTML);
-  const num2 = parseFloat(document.querySelector('.hitbtc_btc').innerHTML);
+  function calculateDiff1() {
+    const num1 = parseFloat(document.querySelector('.poloniex_btc').innerHTML);
+    const num2 = parseFloat(document.querySelector('.hitbtc_btc').innerHTML);
 
-  if (num1 > num2) {
-    document.querySelector('.difference1').innerHTML = num1 - num2;
+    if (num1 > num2) {
+      document.querySelector('.difference1').innerHTML = num1 - num2;
+    }
+    else {
+      document.querySelector('.difference1').innerHTML = num2 - num1;
+
+    }
   }
-  else {
-    document.querySelector('.difference1').innerHTML = num2 - num1;
 
+  document.querySelector('.calculate1').addEventListener('click', calculateDiff1);
+
+  function calculateDiff2() {
+    const num1 = parseFloat(document.querySelector('.poloniex_eth').innerHTML);
+    const num2 = parseFloat(document.querySelector('.hitbtc_eth').innerHTML);
+
+    if (num1 > num2) {
+      document.querySelector('.difference2').innerHTML = num1 - num2;
+    }
+    else {
+      document.querySelector('.difference2').innerHTML = num2 - num1;
+
+    }
   }
-}
 
-document.querySelector('.calculate1').addEventListener('click', calculateDiff1);
+  document.querySelector('.calculate2').addEventListener('click', calculateDiff2);
 
-function calculateDiff2() {
-  const num1 = parseFloat(document.querySelector('.poloniex_eth').innerHTML);
-  const num2 = parseFloat(document.querySelector('.hitbtc_eth').innerHTML);
+  function calculateDiff3() {
+    const num1 = parseFloat(document.querySelector('.poloniex_bch').innerHTML);
+    const num2 = parseFloat(document.querySelector('.hitbtc_bch').innerHTML);
 
-  if (num1 > num2) {
-    document.querySelector('.difference2').innerHTML = num1 - num2;
+    if (num1 > num2) {
+      document.querySelector('.difference3').innerHTML = num1 - num2;
+    }
+    else {
+      document.querySelector('.difference3').innerHTML = num2 - num1;
+
+    }
   }
-  else {
-    document.querySelector('.difference2').innerHTML = num2 - num1;
 
+  document.querySelector('.calculate3').addEventListener('click', calculateDiff3);
+
+  function calculateDiff4() {
+    const num1 = parseFloat(document.querySelector('.poloniex_ltc').innerHTML);
+    const num2 = parseFloat(document.querySelector('.hitbtc_ltc').innerHTML);
+
+    if (num1 > num2) {
+      document.querySelector('.difference4').innerHTML = num1 - num2;
+    }
+    else {
+      document.querySelector('.difference4').innerHTML = num2 - num1;
+
+    }
   }
-}
 
-document.querySelector('.calculate2').addEventListener('click', calculateDiff2);
-
-function calculateDiff3() {
-  const num1 = parseFloat(document.querySelector('.poloniex_bch').innerHTML);
-  const num2 = parseFloat(document.querySelector('.hitbtc_bch').innerHTML);
-
-  if (num1 > num2) {
-    document.querySelector('.difference3').innerHTML = num1 - num2;
-  }
-  else {
-    document.querySelector('.difference3').innerHTML = num2 - num1;
-
-  }
-}
-
-document.querySelector('.calculate3').addEventListener('click', calculateDiff3);
-
-function calculateDiff4() {
-  const num1 = parseFloat(document.querySelector('.poloniex_ltc').innerHTML);
-  const num2 = parseFloat(document.querySelector('.hitbtc_ltc').innerHTML);
-
-  if (num1 > num2) {
-    document.querySelector('.difference4').innerHTML = num1 - num2;
-  }
-  else {
-    document.querySelector('.difference4').innerHTML = num2 - num1;
-
-  }
-}
-
-document.querySelector('.calculate4').addEventListener('click', calculateDiff4);
+  document.querySelector('.calculate4').addEventListener('click', calculateDiff4);
 
 
-$(function() {
+  $(function() {
     $('.smooth').on('click', function(event) {
-        const target = $(this.getAttribute('href'));
-        if (target.length) {
-            event.preventDefault();
-            $('html, body').stop().animate({
-                scrollTop: target.offset().top
-            }, 500);
-        }
+      const target = $(this.getAttribute('href'));
+      if (target.length) {
+        event.preventDefault();
+        $('html, body').stop().animate({
+          scrollTop: target.offset().top
+        }, 500);
+      }
     });
-});
+  });
 
 })();
